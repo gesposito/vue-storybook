d1115d6445b1e1f3616ea9051c35536011d516f7
Gianluca Esposito <g@esposi.to>
2018-04-06 14:49:56 +0200

    Add bootstrap-vue, bootstrap-italia

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,12 @@
 import { configure } from "@storybook/vue";
 
+import "bootstrap-italia/dist/css/bootstrap-italia.css";
+import "bootstrap-italia/dist/css/italia-icon-font.css";
+
 import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+
+Vue.use(BootstrapVue);
 
 const req = require.context("../src/components", true, /\.stories\.js$/);
 
