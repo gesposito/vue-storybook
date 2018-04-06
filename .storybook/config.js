14e6d5cad031df85313463983f4d38c163158de6
Gianluca Esposito <g@esposi.to>
2018-04-06 11:46:31 +0200

    Add Storybook

---
+++ .storybook/config.js
@@ -0,0 +1,11 @@
+import { configure } from "@storybook/vue";
+
+import Vue from "vue";
+
+const req = require.context("../src/components", true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
